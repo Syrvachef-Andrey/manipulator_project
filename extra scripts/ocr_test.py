644d2e81c06ff4480a3026971c585edac200b59e
Андрей <an.sirvacheff@yandex.ru>
2026-04-05 16:02:43 +0500

    feat: add test script for reading text from image

---
+++ extra scripts/ocr_test.py
@@ -0,0 +1,43 @@
+import cv2
+import pytesseract
+import numpy as np
+
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+opened_image = cv2.imread('.\images\img.png')
+
+rotated_opened_image = cv2.rotate(opened_image, cv2.ROTATE_90_COUNTERCLOCKWISE)
+
+# 1. Твои координаты с радара
+pts_src = np.array([
+    [156, 79],
+    [508, 85],
+    [512, 276],
+    [151, 276]
+], dtype=np.float32)
+
+width, height = 350, 200
+
+pts_dst = np.array([
+    [0, 0],
+    [width, 0],
+    [width, height],
+    [0, height]
+], dtype=np.float32)
+
+matrix = cv2.getPerspectiveTransform(pts_src, pts_dst)
+warped_image = cv2.warpPerspective(rotated_opened_image, matrix, (width, height))
+
+gray_image = cv2.cvtColor(warped_image, cv2.COLOR_BGR2GRAY)
+
+binary_image = cv2.adaptiveThreshold(gray_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 21, 5)
+
+text = pytesseract.image_to_string(binary_image, lang='eng+rus', config='--psm 6')
+
+cv2.imshow("warped image", warped_image)
+cv2.imshow("binary image", binary_image)
+cv2.imshow("gray_image", gray_image)
+cv2.imshow("rotated_opened_image", rotated_opened_image)
+
+print(text)
+cv2.waitKey()
+cv2.destroyAllWindows()
